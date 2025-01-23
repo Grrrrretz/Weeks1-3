2397a17e851542542acb86ae734f4e3efd4e9175
Grrrrretz <lhj15733527022@gmail.com>
2025-01-23 15:02:33 -0500

    doding codinggym

---
+++ Assets/Script/S_spawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_spawner : MonoBehaviour
+{
+    public GameObject PopButton;
+    public Sprite[] Button;
+    public SpriteRenderer pop1;
+
+    public Vector3 A1 = new Vector3(Random.Range(-10, 10), Random.Range(-5, 5), Random.Range(-5, 5));
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseposition = Camera.main.WorldToScreenPoint(Input.mousePosition);
+        if(Button.Length ==0)
+        {
+         
+        }
+        else
+        {
+            if (Button.Length <= 5)
+            {
+                SpriteRenderer. = pop1[Random.Range(1,3)];
+            }
+        }
+    }
+}
